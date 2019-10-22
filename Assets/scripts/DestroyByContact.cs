ff6ee746654d21b6fa23897857e1127fa2f19950
jameslu255 <jameslu255@gmail.com>
2019-10-21 20:49:50 -0700

    Aded to github

---
+++ Assets/scripts/DestroyByContact.cs
@@ -0,0 +1,31 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DestroyByContact : MonoBehaviour {
+
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        MusicSource.clip = MusicClip;
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        if (other.tag == "Enemy")
+        {
+            MusicSource.Play();
+            Instantiate(playerExplosion, transform.position, transform.rotation);
+            Instantiate(explosion, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
