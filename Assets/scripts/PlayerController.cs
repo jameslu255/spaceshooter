2280749b3a9f3f1387d6d956acda1207d7cf2215
Eric Lee <erile@ucdavis.edu>
2019-12-06 00:06:23 -0800

    adjust capsule collider to fit better

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -17,8 +17,9 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce (movement * speed);
-	}
+        rb.AddForce(movement * speed, ForceMode.Impulse);
+        //rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
 }
