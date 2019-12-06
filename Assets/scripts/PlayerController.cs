3494e77092542b42474255f773fb7799e18d05d7
Eric Lee <erile@ucdavis.edu>
2019-12-06 00:35:47 -0800

    add ship rotation, improve collider shape, and adjust gun location

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,25 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public float speed;
+    public float speed;
 
-	private Rigidbody rb;
+    private Rigidbody rb;
 
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody>();
-	}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.MoveRotation(Quaternion.Euler(new Vector3(90, 0, -30 * moveHorizontal)));
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
         rb.AddForce(movement * speed, ForceMode.Impulse);
-        //rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
     }
 }
